44594cc5638971fe0c624dd4e6a8dad854920c52
Dave Naugler <davenaugler@gmail.com>
2023-08-10 11:29:09 -0600

    removed white space

--- src/main/SuspectFinder.java
+++ src/main/SuspectFinder.java
@@ -17,31 +17,27 @@ public class SuspectFinder {
 		FileService fileService = new FileService();
 		List<Suspect> suspectLineUp = fileService.getSuspectsFromFile();
 
-		
 		// There should only be one person that is returned here...
 		// The map listed here is the birth year (Integer) and the List of suspects
 		// first filter out all the suspects with red hair...
 		// then filter out all the suspects with glasses
-		// then group by their birth year (Collectors.groupingBy(Suspect::getBirthYear, Collectors.toList())
-		//   -> this will return the birth year and a list of suspect obects (if done correctly, there should
-		//		only be one suspect object.
-		
+		// then group by their birth year (Collectors.groupingBy(Suspect::getBirthYear,
+		// Collectors.toList())
+		// -> this will return the birth year and a list of suspect obects (if done
+		// correctly, there should
+		// only be one suspect object.
+
 		// APPLY FILTERING LOGIC HERE....
 
 		Map<Integer, List<Suspect>> suspects = suspectLineUp.stream()
-			 		 									    .filter(suspect -> !suspect.getHairColor().equals("RED"))
-			 		 									    .filter(suspect -> !suspect.getHasGlasses())
-			 		 									    .collect(Collectors.groupingBy(Suspect::getBirthYear));
+				.filter(suspect -> !suspect.getHairColor().equals("RED")).filter(suspect -> !suspect.getHasGlasses())
+				.collect(Collectors.groupingBy(Suspect::getBirthYear));
 
-			 		 							
-		String guilty = suspects.entrySet()
-				.stream()
-				.map(entry -> entry.getValue().get(0).getName())
-				.findFirst()
+		String guilty = suspects.entrySet().stream().map(entry -> entry.getValue().get(0).getName()).findFirst()
 				.orElse(null);
 
 		System.out.println(guilty + " is guilty");
-		
+
 	}
 
 }
